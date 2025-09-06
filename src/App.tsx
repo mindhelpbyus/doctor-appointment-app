7288ea5f327a0452576597b2ce61996c3e53392b
[dyad] <git@dyad.sh>
2025-09-05 19:27:56 -0700

    [dyad] Fixing page import error - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./components/layout/MainLayout";
-import { Index, NotFound, SearchPage, LoginPage, RegisterPage, AppointmentsPage, AgencyPage } from "./pages"; // Updated import
+import { Index, NotFound, SearchPage, LoginPage, RegisterPage, AppointmentsPage, AgencyPage } from "./pages";
 
 const queryClient = new QueryClient();
 
