3c2574598e93efa8b75bd3e41ac1814b9b66da46
[dyad] <git@dyad.sh>
2025-09-05 19:27:27 -0700

    [dyad] wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
 import MainLayout from "./components/layout/MainLayout";
-import SearchPage from "./pages/SearchPage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import AppointmentsPage from "./pages/AppointmentsPage";
-import AgencyPage from "./pages/AgencyPage";
+import { Index, NotFound, SearchPage, LoginPage, RegisterPage, AppointmentsPage, AgencyPage } from "./pages"; // Updated import
 
 const queryClient = new QueryClient();
 
